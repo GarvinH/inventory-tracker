6e3a8082daafcad3573f299f48ee1d3300188b86
Garvin Hui <garvinh2001@hotmail.com>
2022-01-07 15:57:56 -0500

    Add Create item page

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
     validates :name, presence: true
     validates :description, length: {minimum: 10}, presence: true
-    validates :stock_available, comparison: { greater_than_or_equal_to: 0 }, presence: true
+    validates :stock_avail, comparison: { greater_than_or_equal_to: 0 }, presence: true
     validates :unit_price, comparison: { greater_than: 0 }, presence: true
     validates :stock_sold,  comparison: { greater_than_or_equal_to: 0 }, presence: true
     validates :sell_price, comparison: { greater_than: 0 }, presence: true

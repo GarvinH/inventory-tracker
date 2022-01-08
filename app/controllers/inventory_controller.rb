eef691a80e74816442bc4d1be7ea7908d04b37fd
Garvin Hui <garvinh2001@hotmail.com>
2022-01-07 19:46:00 -0500

    Add item deletion

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -7,28 +7,40 @@ class InventoryController < ApplicationController
     @item = Item.new
   end
 
+  
   def create
     @item = Item.new(item_params)
-
+    
     if @item.save
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
   end
-
+  
   def edit
     @item = Item.find(params[:id])
   end
-
+  
   def update
     @item = Item.find(params[:id])
-
+    
     if @item.update(item_params)
       redirect_to root_path
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+  
+  def confirm_destroy
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "inventory#index"
 
+  get "/inventory/:id/confirm_destroy", to: "inventory#confirm_destroy"
+  
   resources :inventory
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
